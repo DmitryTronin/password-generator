f4847239e7778e6bea8067aef88faa82f1d23964
DmitryTronin <dmitry.tronin@jetbrains.com>
2023-07-04 11:30:10 +0200

    uploaded via github UI

---
+++ app/src/test/java/password/generator/AppTest.java
@@ -0,0 +1,35 @@
+package password.generator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void validUserInput_ShouldResultInExpectedOutput() {
+        String userInput = String.format("Cookie%sMonster%scm@gmail.com",
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+
+        String expected = "Cookie,Monster,cm@gmail.com";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        App.main(null); // call the main method
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actual = lines[lines.length-1];
+
+        // checkout output
+        Assertions.assertEquals(expected,actual);
+    }
+}
