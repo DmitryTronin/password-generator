47469a471e903df2accc98a60c1cbe4f484e21ed
Henri Korhonen <Henri.Korhonen@acme.fi>
2023-07-21 15:16:13 +0200

    doc for main

--- app/src/main/java/password/generator/Main.java
+++ app/src/main/java/password/generator/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
     public static final Scanner keyboard = new Scanner(System.in);
 
+    /**
+     * The main method is the entry point of the application. It instantiates a new object of the Generator class,
+     * passes a Scanner instance, keyboard, to it for user input. Afterwards, it enters the main loop of the
+     * Generator instance, where the application's main operations are performed.
+     * After the operations are done, it closes the Scanner instance to free up system resources.
+     * 
+     * @param args Command-line arguments that are passed to the app when it's launched. Currently not used.
+     */
+    
     public static void main(String[] args) {
         Generator generator = new Generator(keyboard);
         generator.mainLoop();
